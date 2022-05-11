17245510d3715cf9588ba247b2421e920b3a162e
maksim <krovorgen@gmail.com>
2022-05-11 20:56:08 +0300

    fetch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { SyntheticEvent, useCallback } from 'react';
-import axios from 'axios';
 
 const BOT_API_KEY = process.env.NEXT_PUBLIC_BOT_API_KEY;
 const MY_CHANNEL_NAME = '@toastmaster_weddings_germany';
@@ -14,13 +13,12 @@ const Home: NextPage = () => {
       name: { value: string };
       tel: { value: string };
     };
-
     const MY_MESSAGE_TEXT = `Имя: ${name.value} Телефон: ${tel.value}`;
 
-    const data = await axios.get(
+    const res = await fetch(
       `https://api.telegram.org/bot${BOT_API_KEY}/sendMessage?chat_id=${MY_CHANNEL_NAME}&text=${MY_MESSAGE_TEXT}`,
     );
-    console.log(data);
+    console.log(await res.json());
   }, []);
   return (
     <div>
@@ -30,8 +28,8 @@ const Home: NextPage = () => {
         <link rel="icon" href="/public/favicon.ico" />
       </Head>
       <form onSubmit={submit}>
-        <input type="text" placeholder="Ваше имя" name="name" />
-        <input type="tel" placeholder="Введите номер телефона" name="tel" />
+        <input type="text" placeholder="Ваше имя" required name="name" />
+        <input type="tel" placeholder="Введите номер телефона" required name="tel" />
         <button type="submit">Отправить</button>
       </form>
     </div>
